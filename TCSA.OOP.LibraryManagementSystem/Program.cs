f40902bcd160fb98931114bb0807a50f2f7c014c
pinksparrows <jonmaskee@hotmail.com>
2025-04-02 11:47:54 -0400

    Learned about Tuples, replaced Program.cs code with a tuple, though a different strategy is better which I'll be learning soon.

--- TCSA.OOP.LibraryManagementSystem/Program.cs
+++ TCSA.OOP.LibraryManagementSystem/Program.cs
@@ -4,7 +4,10 @@ class Program
 {
     void Main(string[] args)
     {
-        UserInterface userInterface = new();
-        userInterface.MainMenu();
+        //UserInterface userInterface = new();
+        //userInterface.MainMenu();
+
+        (string Title, int Pages) book1 = ("Frankenstein", 350);
+        Console.WriteLine($"Title: {book1.Title}, Pages: {book1.Pages}");
     }
 }
